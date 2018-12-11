e80a0bf60bb9e38856270bba8a9725e3b2fdaaab
dani <mdanielm@protonmail.com>
2018-12-11 20:25:45 +0100

    feature user/profile allow to get funds email and username

--- users/serializers.py
+++ users/serializers.py
@@ -2,13 +2,21 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from printapi.models import File, Folder, History
+from printapi.models import File, Folder, History, Funds
 
 
 class UserSerializer(ModelSerializer):
+
+    funds = serializers.SerializerMethodField()
+
+    def get_funds(self, folder):
+        print(folder.id)
+        query = Funds.objects.all().filter(owner=self.context['request'].user)
+        return query.values('amount')[0]['amount'] if len(query) != 0 else 0.0
+
     class Meta:
         model= User
-        fields= ('id', 'username', 'npages', 'file', )
+        fields= ('id', 'username', 'email', 'funds', )
 
 
 
